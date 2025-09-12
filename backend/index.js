153f7f4d4edd7918ac71bd8e15070d59eff6d456
Sunny Saini <115605667+Sunny5130@users.noreply.github.com>
2025-09-12 13:43:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-nestsaini.vercel.app",
     credentials: true,
   })
 );
